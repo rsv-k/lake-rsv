73df3295f0e1cf2f9e2a2ea146fb9c6e82a9fe19
rsv-k <51999272+rsv-k@users.noreply.github.com>
2019-08-17 13:55:56 +0300

    module removed

--- lake.js
+++ lake.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const lake = new Discord.Client();
 
-
 lake.on('ready', () => { console.log(`Lake has been launched`) });
 lake.on('message', msg => {
     if(!msg.guild || msg.author.bot) return;
@@ -9,24 +8,17 @@ lake.on('message', msg => {
 
     if (text.includes('lago') || text.includes('лаго')) {
         const lago = lake.emojis.find(emoji => emoji.name === 'lago');
-        
         msg.react(lago.id);
     }
 
-    if (text.includes('-ma') ) {
-        toggleMute(msg, true);
-    }
-    else if (text.includes('-ua')) {
-        toggleMute(msg, false);
-    }
+    if (text.includes('-ma')) toggleMute(msg, true);
+    else if (text.includes('-ua')) toggleMute(msg, false);
 });
 
 function toggleMute(msg, value) {
-    msg.delete();
     const channel = msg.member.voiceChannel;
-
     if (!channel || msg.author.id !== '322741339999698955') return;
-
+    msg.delete();
 
     channel.members.forEach(member => {
         if (member.user.id !== msg.author.id) member.setMute(value);
