a110219d647424ae869a6637c9f003c46fc25648
rsv-k <51999272+rsv-k@users.noreply.github.com>
2019-08-17 13:17:28 +0300

    message delete fix

--- lake.js
+++ lake.js
@@ -1,19 +1,33 @@
 const Discord = require('discord.js');
-
 const lake = new Discord.Client();
 
 
 lake.on('ready', () => { console.log(`Lake has been launched`) });
 lake.on('message', msg => {
-    if(!msg.guild || msg.author.bot || msg.author.id === '481189853241802792') return;
+    if(!msg.guild || msg.author.bot) return;
     const text = msg.content.toLowerCase();
 
     if (text.includes('lago') || text.includes('лаго')) {
         const lago = lake.emojis.find(emoji => emoji.name === 'lago');
         
         msg.react(lago.id);
-        console.log(msg.content);
     }
-})
 
+    if (text.includes('-ma') ) {
+        toggleMute(msg, true);
+    }
+    else if (text.includes('-ua')) {
+        toggleMute(msg, false);
+    }
+});
+
+function toggleMute(msg, value) {
+    msg.delete();
+    const channel = msg.member.voiceChannel;
+    if (!channel) return;
+
+    channel.members.forEach(member => {
+        if (member.user.id !== msg.author.id) member.setMute(value);
+    })
+}
 lake.login(process.env.TOKEN);
