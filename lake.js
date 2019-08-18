ab265dd69f1ed0370b666042492888c9437268a8
rsv-k <51999272+rsv-k@users.noreply.github.com>
2019-08-19 02:42:42 +0300

    fix for pics

--- lake.js
+++ lake.js
@@ -5,64 +5,68 @@ const lake = new Discord.Client();
 lake.on('message', async (msg) => {
     if (msg.author.bot) return;
     if (msg.channel.type === 'dm' && msg.author.id !== '502171534543028237') return;
-
     const text = msg.content.toLowerCase();
-    if (Math.floor(Math.random() * 1000) === 7) return addReaction(msg, 'lago');
+
+    try {
+        if (Math.floor(Math.random() * 1000) === 7) return addReaction(msg, 'lago');
     
-    if (text.includes('lago') || text.includes('лаго')) {
-        let reaction = 'lago';
+        if (text.includes('lago') || text.includes('лаго')) {
+            let reaction = 'lago';
 
-        if (Math.floor(Math.random() * 30) === 7) reaction = 'lagotired';
-        else if (Math.floor(Math.random() * 100) === 7) rection = 'lagoscared';
-        return addReaction(msg, reaction);
-    }
+            if (Math.floor(Math.random() * 30) === 7) reaction = 'lagotired';
+            else if (Math.floor(Math.random() * 100) === 7) rection = 'lagoscared';
+            return addReaction(msg, reaction);
+        }
 
-    if (msg.channel.type === 'dm' && msg.author.id === '502171534543028237' && text === 'покажи') {
-        let posts = (await getDataFromReddit('penis'));
-        if (!posts.length) return;
+        if (msg.channel.type === 'dm' && msg.author.id === '502171534543028237' && text === 'покажи') {
+            let posts = (await getDataFromReddit('penis'));
+            if (!posts.length) return;
 
-        posts = posts.filter(post => {
-            return !post.data.url.includes('v.redd.it') 
-            && !post.data.url.includes('reddit.com/r/')
-            && !post.data.url.includes('discord.gg')
-            && !post.data.url.includes('youtu.be');
-        });
+            posts = posts.filter(post => {
+                return !post.data.url.includes('v.redd.it') 
+                && !post.data.url.includes('reddit.com/r/')
+                && !post.data.url.includes('discord.gg')
+                && !post.data.url.includes('youtu.be');
+            });
 
-        const randomPost = Math.floor(Math.random() * (posts.length - 1));
-        const url = posts[randomPost].data.url;
+            const randomPost = Math.floor(Math.random() * posts.length);
+            const url = posts[randomPost].data.url;
+            
+            return msg.channel.send(url);
+        }
+        if (msg.channel.type === 'dm' || text === 'пикчу') return getPicture(msg);
         
-        return msg.channel.send(url);
-    }
-    if (msg.channel.type === 'dm' || text === 'пикчу') return getPicture(msg);
-    
-    if (msg.channel.type !== 'dm' && msg.channel.name.toLowerCase().includes('голосование')) {
-        const text = msg.content;
-        const numbers = text.match(/\d+./g);
-        if (!numbers || !text.toLowerCase().includes('темы')) return;
+        if (msg.channel.type !== 'dm' && msg.channel.name.toLowerCase().includes('голосование')) {
+            const text = msg.content;
+            const numbers = text.match(/\d+./g);
+            if (!numbers || !text.toLowerCase().includes('темы')) return;
 
-        const title = text.split('\n')[0];
-        let body = '';
-        text.split('\n').forEach((item, i) => {
-            body += i !== 0 ? item + '\n' : '';
-        })
-        
-        const embed = new Discord.RichEmbed()
-        .setTitle(title)
-        .setColor(0x00000)
-        .setDescription(body);
-        msg.reply({embed}).then(async (message) => {
-            const lastNumber = parseInt(numbers[numbers.length - 1]);
-            const emojiNumbers = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣'];
+            const title = text.split('\n')[0];
+            let body = '';
+            text.split('\n').forEach((item, i) => {
+                body += i !== 0 ? item + '\n' : '';
+            })
+            
+            const embed = new Discord.RichEmbed()
+                .setTitle(title)
+                .setColor(0x00000)
+                .setDescription(body);
+                msg.reply({embed}).then(async (message) => {
+                    const lastNumber = parseInt(numbers[numbers.length - 1]);
+                    const emojiNumbers = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣'];
 
-            for(let i = 0; i < lastNumber; i++) {
-                await message.react(`${emojiNumbers[i]}`);
+                    for(let i = 0; i < lastNumber; i++) {
+                        await message.react(`${emojiNumbers[i]}`);
+                    }
+                });
+                msg.delete();
             }
-        });
-        msg.delete();
-    }
 
-    if (text.includes('-ma')) toggleMute(msg, true);
-    else if (text.includes('-ua')) toggleMute(msg, false);
+        if (text.includes('-ma')) toggleMute(msg, true);
+        else if (text.includes('-ua')) toggleMute(msg, false);
+    }
+    catch(err) { console.error(err) }
+    
 });
 
 function toggleMute(msg, value) {
@@ -81,7 +85,7 @@ function addReaction(msg, reaction) {
 
 async function getPicture(msg) {
     const communities = ['funny', 'gifs', 'reactiongifs', 'pics', 'aww', 'PrettyGirls', 'interestingasfuck', 'AdviceAnimals', 'food', 'PeopleFuckingDying', 'rarepuppers', 'NatureIsFuckingLit', 'BeAmazed', 'Tinder', 'natureismetal', 'perfectlycutscreams', 'EarthPorn', 'Anime', 'oddlysatisfying', 'Wellthatsucks','popular', 'mildlyinteresting', 'original', 'meme', 'dank_meme'];
-    const randomCommunity = Math.floor(Math.random() * (communities.length - 1));
+    const randomCommunity = Math.floor(Math.random() * communities.length);
     let posts = (await getDataFromReddit(communities[randomCommunity]));
     
     if (!posts.length) return;
@@ -92,15 +96,13 @@ async function getPicture(msg) {
         && !post.data.url.includes('youtu.be');
     });
 
-    const randomPost = Math.floor(Math.random() * (posts.length - 1));
+    const randomPost = Math.floor(Math.random() * posts.length);
     const url = posts[randomPost].data.url;
     let embed = new Discord.RichEmbed();
-    if (url.includes('i.redd.it') || url.includes('.jpg') || url.includes('.png')) {
-        embed.setImage(url);
-    }
+    if (url.includes('i.redd.it') || url.includes('.jpg') || url.includes('.png')) embed.setImage(url);
     else embed = url;
     
-    msg.channel.send(embed);
+    return msg.channel.send(embed);
 }
 async function getDataFromReddit(community) {
     return (await snekfetch.get(`https://www.reddit.com/r/${community}.json?sort=top&t=week`).query(200)).body.data.children;
