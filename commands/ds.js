a2c7d42f2b8fe6657b6ad153c46a04c2f0a327ae
rsv-k <51999272+rsv-k@users.noreply.github.com>
2019-09-15 13:55:36 +0300

    forbid disconnecting bots

--- commands/ds.js
+++ commands/ds.js
@@ -1,17 +1,29 @@
 exports.run = (msg, args) => {
-    const members = [...args].filter(Boolean);
-    if (!members[0] || !msg.member.roles.find(role => role.name === 'Цензор' || role.name === 'Редактор')) return;
+    /* 
+        Filter out all bots
+    */
+    const members = args.filter(id => {
+        const user = getMember(msg, id);
+
+        return !user.user.bot;
+    });
+
+    if (!members[0] || !msg.member.roles.find(role => role.name === 'Цензор' || role.name === 'Редактор')) return msg.reply('invalid user');
     
     msg.guild.createChannel('disconnect', {type: 'voice'})
     .then (async (channel) => {
-        for (let i = 0; i < members.length; i++) {
-            const id = members[i].match(/\d+/)[0];
-            const member = msg.guild.members.get(id);
 
+        for (let i = 0; i < members.length; i++) {
+            const member = getMember(msg, members[i]);
+            
             if (!member.voiceChannel) continue;
             await member.setVoiceChannel(channel.id);
         }
 
         await channel.delete();
     });
+}
+
+function getMember(msg, id) {
+    return msg.guild.members.get(id.match(/\d+/)[0]);
 }
