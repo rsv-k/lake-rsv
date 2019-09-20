cc089788346608b3629eba67fc1e4bc6df87c7c3
rsv-k <51999272+rsv-k@users.noreply.github.com>
2019-09-20 20:37:10 +0300

    server added

--- commands/s.js
+++ commands/s.js
@@ -1,18 +1,17 @@
 exports.run = (msg, args, guildMusic) => {
     const playlist = guildMusic.get(msg.guild.id);
+    console.log(playlist.dispatcher.dispatcher);
     const amount = args[0] || 1;
+
     if(!msg.guild.voiceConnection || playlist.songs.length === 0) return msg.reply('Nothing is being played.');
     if(!amount || amount > playlist.songs.length || amount < 1 || !Number(amount)) 
         return msg.reply('Can not skip that amount');
 
-    if (amount === 1) {
-        playlist.dispatcher.dispatcher.end();
-    }
-    else if (amount > 1) {
+    if (amount > 1) {
         playlist.songs = playlist.songs.splice(amount - 1);
         guildMusic.set(msg.guild.id, playlist);
-        playlist.dispatcher.dispatcher.end();
     }
 
+    playlist.dispatcher.dispatcher.end();
     msg.channel.send(`${amount} ${ amount > 1 ? 'songs have' : 'song has'} been skipped`);
 }
